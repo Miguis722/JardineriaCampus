c79514a6b10daf03b29c00e626ae326fcde62ad4
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-04 06:50:54 -0500

    Explicación

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import modules.getClients as logic
 
+logic.cli.clientes.append({"nombre": "prueba"})
 print(logic.cli.clientes)
+#Podemos observar que EFECTIVAMENTE, lo agrego a cliente
+#Pero no donde queremos, sino que lo guardo en el archivo CACHÉ
