8aaf26df28fbfc3e2e149128c52b1f15d2296939
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-04 06:29:56 -0500

    Commit numero 1 del ejercicio

---
+++ Storage/producto.py
@@ -0,0 +1,13 @@
+producto = [
+    {
+        "codigo_producto": "",
+        "nombre": "",
+        "gama": "",
+        "dimensiones": "",
+        "proovedor": "",
+        "descripcion": "",
+        "cantidad_en_stock": "",
+        "precio_venta": "",
+        "precio_proovedor": ""
+    }
+]
