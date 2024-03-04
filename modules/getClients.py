43ce7d6530bf3cdf72f0f5c907ebcbdc772176b7
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-04 07:01:59 -0500

    explicacion

--- modules/getClients.py
+++ modules/getClients.py
@@ -2,7 +2,10 @@ import Storage.cliente as cli
 
 def search():
     clienteNames = list()
-    for i,val in enumerate(cli.clientes):
-        clienteNames.append(val.get('nombre_cliente'))
+    for val in cli.clientes:
+    #No es necesario poner indice y enumerate
+        clienteNames.append({
+            "codigo_cliente": val.get('codigo_cliente'),
+            "nombre_cliente": val.get('nombre_cliente')})
     return clienteNames
 #Estamos pidiendo solamente los nombres de las personas
