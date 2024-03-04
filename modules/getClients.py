8737ebb7dc05af98a297f4461df12dccd426805f
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-04 07:16:27 -0500

    Añadimiento de codigo para obtener un solo nombre por codigo

--- modules/getClients.py
+++ modules/getClients.py
@@ -11,3 +11,12 @@ def search():
         clienteNames.append(codigoName)
     return clienteNames
 #Estamos pidiendo solamente los nombres de las personas
+
+def getOneClienteCodigo(codigo):
+    for val in cli.clientes:  
+        if(val.get('codigo_cliente') == codigo):
+            return{
+            "codigo_cliente": val.get('codigo_cliente'),
+            "nombre_cliente": val.get('nombre_cliente')
+            }
+#Aqui haremos que solo nos de un nombre por codigo
