06106f61d9f97989395566887fa2b298f27edbea
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-07 08:10:33 -0500

    Corrección del error de pedido

--- modules/getPedido.py
+++ modules/getPedido.py
@@ -14,22 +14,24 @@ def getAllProcesoPedido():
 #Código de cliente, fecha esperada y fecha de
 #Entrega de los pedidos que no han sido entregados.
 
+
 def getAllPedidosEntregadosAtrasadosDeTiempo():
     pedidosEntregados = []
     for val in ped.pedido:
-        if val.get("estado") == "Entregado" and val.get("fecha_entregada") is None:
-            val["fecha_entregada"] = val.get("fecha_esperada")
         if val.get("estado") == "Entregado":
-            date_1 = "/".join(val.get("fecha_entrega").split("-")[::-1])
-            date_2 = "/".join(val.get("fecha_esperada").split("-")[::-1])
-            start = datetime.strptime(date_1, "%d/%m/%Y")
-            end = datetime.strptime(date_2, "%d/%m/%Y")
-            diff = end.date() - start.gate()
-        if(diff.gays < 0):
-            pedidosEntregados.append({
-            "codigo_de_pedido": val.get("codigo_pedido"),
-            "codigo_de_cliente": val.get("codigo_cliente"),
-            "fecha_esperada": val.get("fecha_esperada"),
-            "fecha_entrega": val.get("fecha_entrega")
-        })
+            fecha_entrega = val.get("fecha_entrega")
+            fecha_esperada = val.get("fecha_esperada")
+            if fecha_entrega is not None and fecha_esperada is not None:
+                date_1 = "/".join(fecha_entrega.split("-")[::-1])
+                date_2 = "/".join(fecha_esperada.split("-")[::-1])
+                start = datetime.strptime(date_1, "%d/%m/%Y")
+                end = datetime.strptime(date_2, "%d/%m/%Y")
+                diff = end.date() - start.date()
+                if diff.days < 0:
+                    pedidosEntregados.append({
+                        "codigo_de_pedido": val.get("codigo_pedido"),
+                        "codigo_de_cliente": val.get("codigo_cliente"),
+                        "fecha_esperada": fecha_esperada,
+                        "fecha_entrega": fecha_entrega
+                    })
     return pedidosEntregados
