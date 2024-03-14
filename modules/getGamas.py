cac8b5c7b9e7d8c02f526eaffd3fdd53b9b6c0d2
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-14 09:00:13 -0500

    Desarrollo del servidor de Gamas

--- modules/getGamas.py
+++ modules/getGamas.py
@@ -1,1 +1,13 @@
 import requests
+
+#Servidor de Gamas
+def getAllDataGamas():
+    peticion = requests.get("http://172.16.106.112:5007")
+    data = peticion.json()
+    return data
+
+def getAllNombre():
+    gamaNombre = []
+    for val in getAllDataGamas():
+        gamaNombre.append(val.get("gama"))
+    return gamaNombre
